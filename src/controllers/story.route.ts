094b3c1965569215110860f955528c2dcd5e7b1b
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-18 18:45:20 +0700

    add types

--- src/controllers/story.route.ts
+++ src/controllers/story.route.ts
@@ -1,13 +1,14 @@
 import { json } from 'body-parser';
 import { Router } from 'express';
 import { Story } from '../models/Story';
+import { RequestWithUser } from '../types';
 import { userMiddleware } from './userMiddleware';
 
 export const storyRoute = Router();
 
 storyRoute.use(json());
 
-storyRoute.post('/', userMiddleware, (req: any, res) => {
+storyRoute.post('/', userMiddleware, (req: RequestWithUser, res) => {
     Story.addStory(req.userId, req.body.content)
     .then(story => res.send({ success: true, story }))
     .catch(error => res.status(404).send({ success: false, error: error.message }));
